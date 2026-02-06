aadff936463419ad9417c6d73e234e9fcafaa34e
ferdyciputra <ferdyarmstrong@gmail.com>
2026-02-06 12:21:53 +0700

    add screenshot and video when test failed

--- playwright.config.js
+++ playwright.config.js
@@ -29,8 +29,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: "https://opensource-demo.orangehrmlive.com",
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "retain-on-failure",
   },
 
   expect: {
